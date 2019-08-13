23758ad1ed29286bc09a1b16783accfc59dd5e7c
TylerCEdge <tylercedge@gmail.com>
2019-08-13 11:59:57 -0400

    graphql rendering data from markdown files successfully in blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,38 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 export default function blog() {
+    
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                        html
+                        excerpt
+                    }
+                }
+            }
+        }
+    `)
     return (
         <Layout>
         <h2>Blog</h2>
-        <p>Blog bloggidy blog blog</p>
+        <ol>
+            {data.allMarkdownRemark.edges.map((edge) => {
+                return (
+                    <li>
+                        <h2>{edge.node.frontmatter.title}</h2>
+                        <p>{edge.node.frontmatter.date}</p>
+                    </li>
+                )
+            })}
+        </ol>
         <Link to='/'>Home</Link>
         </Layout>
     )
