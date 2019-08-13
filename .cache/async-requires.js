2687bcb2ee06a765d90fee0b468cf55454c6030a
TylerCEdge <tylercedge@gmail.com>
2019-08-12 23:27:50 -0400

    added scss plugin and a custom stylesheet/ fixed what I broke with vscode terminal to push to github

---
+++ .cache/async-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("C:\\Users\\tyler\\repo\\gatsbypractice\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("C:\\Users\\tyler\\repo\\gatsbypractice\\src\\pages\\about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-blog-js": () => import("C:\\Users\\tyler\\repo\\gatsbypractice\\src\\pages\\blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
+  "component---src-pages-contact-js": () => import("C:\\Users\\tyler\\repo\\gatsbypractice\\src\\pages\\contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("C:\\Users\\tyler\\repo\\gatsbypractice\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
